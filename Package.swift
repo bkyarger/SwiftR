040b2af272d9bde1cac23f47a1a049cd306bb798
Brian Yarger <byarger@continuous-code.com>
2022-06-09 10:55:54 -0700

    created new swift package manager structure, copied iOS source into structure, removed all mac source

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftR",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftR",
             targets: ["SwiftR"]),
@@ -17,11 +17,26 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftR",
-            path: "SwiftR",
-            dependencies: []),
-    ],
-    swiftLanguageVersions: [.v4, .v4_2, .v5]
+            dependencies: [],
+            resources:[
+                .copy("Web/SwiftR.js"),
+                .copy("Web/jquery-2.1.3.min.js"),
+                .copy("Web/jquery.signalr-2.0.0.min.js"),
+                .copy("Web/jquery.signalr-2.0.1.min.js"),
+                .copy("Web/jquery.signalr-2.0.2.min.js"),
+                .copy("Web/jquery.signalr-2.0.3.min.js"),
+                .copy("Web/jquery.signalr-2.1.0.min.js"),
+                .copy("Web/jquery.signalr-2.1.1.min.js"),
+                .copy("Web/jquery.signalr-2.1.2.min.js"),
+                .copy("Web/jquery.signalr-2.2.0.min.js"),
+                .copy("Web/jquery.signalr-2.2.1.min.js"),
+                .copy("Web/jquery.signalr-2.2.2.min.js"),
+            ]),
+        .testTarget(
+            name: "SwiftRTests",
+            dependencies: ["SwiftR"]),
+    ]
 )
---
+++ Tests/SwiftRTests/SwiftRTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftR
+
+final class SwiftRTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+//        XCTAssertEqual(SwiftR().text, "Hello, World!")
+    }
+}
